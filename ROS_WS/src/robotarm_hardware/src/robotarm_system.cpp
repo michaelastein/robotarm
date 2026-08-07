b98d3a8f0fa5bd9ad2cc1903ee116e34821c98f1
michaelastein <michaela.stein@student.uni-luebeck.de>
2026-08-07 15:56:01 +0200

    Update robotarm_system.cpp

--- ROS_WS/src/robotarm_hardware/src/robotarm_system.cpp
+++ ROS_WS/src/robotarm_hardware/src/robotarm_system.cpp
@@ -770,7 +770,7 @@ hardware_interface::return_type RobotArmSystem::write(
      */
     if (info_.joints[i].name == "base_joint")
     {
-      constexpr double base_start_pwm = 0.23;
+      constexpr double base_start_pwm = 0.28;
       constexpr double base_stopped_velocity_rad_s = 0.015;
 
       const bool command_was_stopped =
