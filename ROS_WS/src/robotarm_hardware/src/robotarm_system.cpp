3eaab7d976924cc64ad9a9f4c879f5dd77fc1b13
michaelastein <michaela.stein@student.uni-luebeck.de>
2026-08-08 10:43:11 +0200

    Update robotarm_system.cpp

--- ROS_WS/src/robotarm_hardware/src/robotarm_system.cpp
+++ ROS_WS/src/robotarm_hardware/src/robotarm_system.cpp
@@ -785,8 +785,8 @@ hardware_interface::return_type RobotArmSystem::write(
        * last_active_command_time_ is intentionally checked before it is updated
        * at the end of this write cycle.
        */
-      constexpr double base_true_start_pwm = 0.26;
-      constexpr double base_trim_start_pwm = 0.19;
+      constexpr double base_true_start_pwm = 0.28;
+      constexpr double base_trim_start_pwm = 0.21;
       constexpr double base_true_start_idle_s = 0.30;
       constexpr double base_stopped_velocity_rad_s = 0.005;
 
